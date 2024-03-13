80c1f8cbebb468eaebdbec3bdf69745c15bf6489
Denis <54939243+DenisKrv@users.noreply.github.com>
2024-03-13 09:58:29 +0200

    Commit from TeleportHQ

--- src/index.js
+++ src/index.js
@@ -13,11 +13,12 @@ import CommercialPropertiesGR from './views/commercial-properties-gr'
 import Rodon8Events from './views/rodon8-events'
 import ResidentialPropertiesGR from './views/residential-properties-gr'
 import PropertiesHomeGR from './views/properties-home-gr'
-import Page from './views/page'
 import ContactGR from './views/contact-gr'
 import PropertiesHomeEN from './views/properties-home-en'
 import CommercialPropertiesEN from './views/commercial-properties-en'
 import Rodon8Main from './views/rodon8-main'
+import Page from './views/page'
+import NotFound from './views/not-found'
 
 const App = () => {
   return (
@@ -40,7 +41,6 @@ const App = () => {
           path="/residential-properties-gr"
         />
         <Route component={PropertiesHomeGR} exact path="/properties-home-gr" />
-        <Route component={Page} path="**" />
         <Route component={ContactGR} exact path="/contact-gr" />
         <Route component={PropertiesHomeEN} exact path="/properties-home-en" />
         <Route
@@ -49,6 +49,8 @@ const App = () => {
           path="/commercial-properties-en"
         />
         <Route component={Rodon8Main} exact path="/" />
+        <Route component={Page} exact path="/page" />
+        <Route component={NotFound} path="**" />
         <Redirect to="**" />
       </Switch>
     </Router>
